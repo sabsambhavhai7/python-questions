0929869830f31fc475631ca0e5d26544a4a1ea4e
sabsambhavhai7 <sambhavs@op.iitg.ac.in>
2026-01-31 01:37:56 +0530

    avg , area etc problems

--- python_ques _bank1.py
+++ python_ques _bank1.py
@@ -1,1 +1,35 @@
-print("github practice start now")
+# print("github practice start now")
+
+CHAPTER 1 - PYTHON BASICS QUESTIONS
+
+
+# question - 1 
+a = input ("number 1: A ")
+b = input ("number 2: B ")
+SUM = int(a) + int(b)
+print("The sum of A and B is: ", SUM)   
+
+
+# question - 2
+side_of_square = float(input("Enter the side of square: "))
+area= float(side_of_square) ** 2
+print("The area of square is: ", area)
+
+
+# question - 4
+A= float(input("Enter first number: "))
+B= float(input("Enter second number: "))
+average= (A + B) / 2
+print("The average of two numbers is: ", average)
+
+
+#   question - 4
+A=float(input("Enter first number: "))
+B=float(input("Enter second number: "))
+if A >= B :
+    print("true")
+elif A <= B :
+    print("false")
+
+
+
