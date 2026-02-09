bb06132cc14a1a5baf052ce22d26100834892b7a
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-02-10 00:19:21 +0530

    Modify list1 by removing and popping elements
    Remove element 3 and pop the last element from list1.

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -143,3 +143,8 @@ list1.sort(reverse=True)
 print(list1) # [5, 4, 3, 2, 1]
 list1.reverse()
 print(list1) # [5, 4, 3, 2, 1]
+list1 = [1, 2, 3, 4, 5]
+list1.remove(3)
+print(list1)
+list1.pop(4)
+print(list1)
