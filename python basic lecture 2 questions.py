25a26b2494e2fdc4ade9479e787ec6c9446a2292
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-02-12 23:55:16 +0530

    Update python basic lecture 2 questions.py

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -296,3 +296,12 @@ while i < len(numbser):
     print(numbser[i])
     i += 1 
 
+umbser = (1,4,9,16,25,36,49,64,81,100)
+x=36 
+i=0
+while i < len(numbser):
+    if numbser[i] == x:
+        print("Found")
+        break
+    i += 1 
+
