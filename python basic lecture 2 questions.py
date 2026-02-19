88a4c68d00b5c2102562a6341553167fa22db817
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-02-19 22:44:50 +0530

    Update python basic lecture 2 questions.py

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -513,3 +513,10 @@ for i in range(1, n+1):
 print(fact)
 cal_factorial(5)
 
+# RECURSION
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+print (factorial(5)) 
