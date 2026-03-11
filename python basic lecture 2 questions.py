95f43a1decc67e00b15fabff243ae63d764b67bf
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-03-11 22:24:39 +0530

    Update python basic lecture 2 questions.py

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -1667,3 +1667,8 @@ print(user_info["role"])
 numbers = [1, 2, 3, 4, 5]
 squares = [x**2 for x in numbers]
 print(squares) # [1, 4, 9, 16, 25]
+
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("Oops! You can't divide by zero.")
