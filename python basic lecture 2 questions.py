a2b02337ecd9a5d0ce9c5c646e141da840dd711e
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-03-13 23:55:16 +0530

    Update python basic lecture 2 questions.py

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -1798,3 +1798,8 @@ doubled = list(map(lambda x: x * 2, nums))
 
 price = 49.99
 print(f"The total is ${price * 1.05:.2f}") # Math and rounding in one line
+
+names = ["Alice", "Bob"]
+scores = [85, 92]
+combined = list(zip(names, scores))
+# Output: [('Alice', 85), ('Bob', 92)]
