85a2d7a934fa23741a3f4f230b129d5a0b930414
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-03-17 22:42:21 +0530

    Update python basic lecture 2 questions.py

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -1964,3 +1964,8 @@ def say_hi():
     print("Hi!")
 
 say_hi()
+
+# Goal: Get squares of even numbers from 0 to 9
+numbers = range(10)
+squares = [x**2 for x in numbers if x % 2 == 0]
+print(squares)  # Output: [0, 4, 16, 36, 64]
