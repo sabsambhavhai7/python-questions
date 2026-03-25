875ed28cd40d22f1e38fdba96f1e8233cb78d147
SAMBHAV SHUKLA <sambhavs@op.iitg.ac.in>
2026-03-25 21:02:19 +0530

    Update python basic lecture 2 questions.py

--- python basic lecture 2 questions.py
+++ python basic lecture 2 questions.py
@@ -2774,3 +2774,18 @@ def master_function(*args, **kwargs):
     print(f"Keyword: {kwargs}")   # Dictionary
 
 master_function(1, 2, 3, mode="admin", debug=True)
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y})"
+
+v1 = Vector(2, 4)
+v2 = Vector(1, 3)
+print(v1 + v2) # Output: Vector(3, 7)
